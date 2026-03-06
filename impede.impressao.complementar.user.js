3cf524437a664b5dfe99caae8048e130f222b84f
lksoumon <99751496+lksoumon@users.noreply.github.com>
2026-03-06 08:05:14 -0400

    Update impede.impressao.complementar.user.js

--- impede.impressao.complementar.user.js
+++ impede.impressao.complementar.user.js
@@ -1,1 +1,24 @@
 
+// ==UserScript==
+// @name         Não imprimir calendário
+// @namespace    http://tampermonkey.net/
+// @version      2026-02-26
+// @description  Bloqueia a impressão automática
+// @author       You
+// @match        http://sigeduca.seduc.mt.gov.br/grh/hwmgrhcalendarioimp.aspx?*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=gov.br
+// @grant        unsafeWindow
+// @run-at       document-start
+// @updateURL    https://github.com/lksoumon/acompanhamentoDiarioGEDeFICAI/raw/refs/heads/main/impede.impressao.complementar.user.js
+// @downloadURL  https://github.com/lksoumon/acompanhamentoDiarioGEDeFICAI/raw/refs/heads/main/impede.impressao.complementar.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Sobrescreve a função nativa de impressão na janela principal antes que a página carregue
+    unsafeWindow.print = function() {
+        console.log("Impressão automática bloqueada pelo Tampermonkey.");
+    };
+
+})();
